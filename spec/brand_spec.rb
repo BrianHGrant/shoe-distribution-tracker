bfe0653bcbca52ce40ffe8db557a37ea4aeabe62
BrianHGrant <bhgrant8@gmail.com>
2016-07-22 08:50:17 -0700

    add validation to ensure a value is entered for brand name and passing spec

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -10,4 +10,9 @@ describe(Brand) do
       expect(test_brand.stores()).to(eq([test_store, test_store2]))
     end
   end
+
+  it('ensures that the value entered for Brand is not an empty string') do
+    test_brand = Brand.new({:name => ''})
+    expect(test_brand.save()).to(eq(false))
+  end
 end
